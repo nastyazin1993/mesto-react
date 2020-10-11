4801cc8ebcf6722c988db80d58b5cfd53327a59d
Anastasiia Zinchenko <nastyagun1993@yandex.ru>
2020-10-11 19:28:56 +0300

    Updates

---
+++ precache-manifest.7371be76593ddc380eba494902b0c89a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ce562f34944f91cb6e8d8e012920863",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24e11f4fe60cbd85244c",
+    "url": "/static/css/main.dbe4e76f.chunk.css"
+  },
+  {
+    "revision": "30416706a367a49eb6cb",
+    "url": "/static/js/2.859191b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.859191b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24e11f4fe60cbd85244c",
+    "url": "/static/js/main.fc77a88f.chunk.js"
+  },
+  {
+    "revision": "c5e7b408ed1922744b94",
+    "url": "/static/js/runtime-main.e8402bce.js"
+  },
+  {
+    "revision": "b2fce4ac45f3d116cd48ec8d068ab723",
+    "url": "/static/media/AddButton.b2fce4ac.svg"
+  },
+  {
+    "revision": "c2d0901f14de58acae456bda23f80e8a",
+    "url": "/static/media/CloseButton.c2d0901f.svg"
+  },
+  {
+    "revision": "efa6fb04512d4ea565aaa4109835b46d",
+    "url": "/static/media/DeleteButton.efa6fb04.svg"
+  },
+  {
+    "revision": "6bfdb35467f51d3ba1f70f8d4a9c2b59",
+    "url": "/static/media/EditButton.6bfdb354.svg"
+  },
+  {
+    "revision": "09f4068bb74fc78ffbce74f5596d6aa5",
+    "url": "/static/media/Inter-Black.09f4068b.woff2"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "/static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "7a8cc7241f766a142e15b2948804e547",
+    "url": "/static/media/Inter-Medium.7a8cc724.woff"
+  },
+  {
+    "revision": "f6cf0a0bc5fce3307e2c426eb14eb752",
+    "url": "/static/media/Inter-Medium.f6cf0a0b.woff2"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "/static/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "/static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "9fdebb6b009c16af50fae58657e30a94",
+    "url": "/static/media/heart-black.9fdebb6b.svg"
+  },
+  {
+    "revision": "a8acc871456b1001597672e02c5199ca",
+    "url": "/static/media/heart.a8acc871.svg"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "/static/media/iconEditAvatar.d476a8b8.svg"
+  },
+  {
+    "revision": "855a8c9865e79463f31e74f9abcb4438",
+    "url": "/static/media/logo.855a8c98.svg"
+  }
+]);
